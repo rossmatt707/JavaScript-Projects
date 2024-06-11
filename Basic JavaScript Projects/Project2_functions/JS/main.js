c4d79b099d49654a09611f2baff1391d79b242ee
rossmatt707 <rossmat707@gmail.com>
2024-06-11 09:03:19 -0700

    Comments
    Added comments to function files

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,11 +1,11 @@
 function Click_It() { //This is defining the function
-    var str = "Yes you can. One hundred cheeseburgers please.";
-    document.getElementById("Button_Text").innerHTML = str;
+    var str = "Yes you can. One hundred cheeseburgers please."; //The string that replaces the original html text
+    document.getElementById("Button_Text").innerHTML = str; //accesses the html file and replaces the text with our new variable.
 }
 
-function ConcatenateFunction() {
-    var sentence = "At the corner of";
-    sentence += " 5th and main street.";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function ConcatenateFunction() { //This is defining the function
+    var sentence = "At the corner of"; //The first part of the variable
+    sentence += " 5th and main street."; //The second part that will be concatenated
+    document.getElementById("Concatenate").innerHTML = sentence; //This accesses the html file and replaces the text tied to the ID "Concatenate" with our new concenated message.
 }
 
