d9aa75d25b76a86cbf3ce6b468617b63f59c663d
rossmatt707 <rossmat707@gmail.com>
2024-06-10 17:00:05 -0700

    Added basic JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+Sent1 = "This is the beginning of the string" //defining a variable
+Sent2 = " and this is the end of the string" //defining a variable    
+document.write(Sent1 + Sent2); //concenating the two prev defined variables.
+
+document.write(1+53); //writing an expression
+
+function My_First_function() { //This is defining the function
+    var str = "this is the button text!";
+    document.getElementId("Button_Text").innerHTML = str;
+}
