19599ecdea342c659d5110dfd7edb3f656abcaf2
rossmatt707 <rossmat707@gmail.com>
2024-06-11 08:54:56 -0700

    Added functions project
    new folder and project

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function Click_It() { //This is defining the function
+    var str = "Yes you can. One hundred cheeseburgers please.";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function ConcatenateFunction() {
+    var sentence = "At the corner of";
+    sentence += " 5th and main street.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+
